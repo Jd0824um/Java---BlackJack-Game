80d20560ba7450b0967b7c0838496066c5ae221a
John Zika <johnzikajr@outlook.com>
2019-03-06 19:57:57 -0600

    Worked on more validiation. Created a method for creating the shoe. TODO redo validation on howManyPlayers() method

--- src/Game.java
+++ src/Game.java
@@ -9,16 +9,17 @@ import java.util.Scanner;
  */
 public class Game {
 
-	private Validation validation;
+	private static Validation validation = new Validation();
 	private ArrayList<Player> players;
+	private static Shoe shoe;
 	
 	public static void main(String[] args) {
-		Shoe shoe = new Shoe(2, true);
-		
+		createShoe(numberOfDecks(), true);
 		for (Card card : shoe.getShoe()) {
 			System.out.println(card);
 		}
 	}
+
 	
 
 	
@@ -32,13 +33,13 @@ public class Game {
 		if (validation.isInt(numPlayers)) { // Checks to see if it is an int.
 			
 			for (int p = 0; p < Integer.parseInt(numPlayers); p++) {
-				System.out.println("Enter your name player " + p + 1 + " !");				
+				System.out.println("Enter your name player " + p + 1 + " !\n");				
 				Scanner scanner = new Scanner(System.in);	
 				String name = scanner.nextLine(); // Gets the name of the player.
 				
-				while (!validation.nameLength(name)) {
-					System.out.println("Name is to long. Please keep it at 8 characters or less!");
-					System.out.println("Please enter your name player " + p + 1 + " !");
+				while (!validation.playerValidation(name)) { // While invalid information exists, this continues to loop.
+					System.out.println("Not a valid name. Please keep it at 8 characters or less!\n");
+					System.out.println("Please enter your name player " + p + 1 + " !\n");
 					name = scanner.nextLine();		
 				}
 				
@@ -46,14 +47,34 @@ public class Game {
 				player.setName(name); // Sets the player's name to the corresponding input.
 				players.add(player); // Adds the player to the ArrayList.
 				
-				System.out.println("Welcome " + player.getName() + " !");
+				System.out.println("Welcome " + player.getName() + " !\n");
 				
 				scanner.close(); // Closes the scanner.
 			}
 		}
 	}
 	
-	public void createShoe(int numDecks, boolean shuffle) {
+	/**
+	 * A method to create decks based on the users input.
+	 * @return
+	 */
+	public static String numberOfDecks() {
+		System.out.println("How many decks do you want to use? (Max 8 decks in a shoe.)\n");
+		Scanner scanner = new Scanner(System.in);
+		String numDecks = scanner.nextLine();
 		
+		while (!validation.deckValidation(numDecks)) { // While invalid information exists, this continues to loop.
+			System.out.println(numDecks.toString() + " is not valid.\n");
+			System.out.println("Please make sure the number is within range (1 - 8), not empty, and a whole number.\n");
+			System.out.println("How many decks wouldyou like to use?\n ");
+			numDecks = scanner.nextLine();
+		}
+		
+		scanner.close();
+		return numDecks;
+	}
+	
+	public static void createShoe(String numDecks, boolean shuffle) {
+		shoe = new Shoe(Integer.parseInt(numDecks), shuffle);
 	}
 }
--- src/Validation.java
+++ src/Validation.java
@@ -1,12 +1,36 @@
-/**\
- * A class to handle all the validation of the game.
+/**
+ * A class to handle all the validation checks of the game.
  * 
  * 
  * @author John Zika
  *
  */
 public class Validation {
-
+	
+	/**
+	 * Checks to see that the deck being created is valid.
+	 * @param number
+	 * @return
+	 */
+	public boolean deckValidation(String number) {
+		if (isInt(number) && isEmpty(number) && isInRange(number)) {
+			return true;			
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks if the inputed string is not to large.
+	 * @param name
+	 * @return
+	 */
+	public boolean playerValidation(String name) {
+		if (isInRange(name) && isEmpty(name)) {
+			return true;
+		}
+		return false;
+	}
+	
 	/**
 	 * Checks to see if the inputed string is a int.
 	 * @param number
@@ -23,16 +47,26 @@ public class Validation {
 	}
 	
 	/**
-	 * Checks if the inputed string is not to large.
-	 * @param name
+	 * Checks to see if the number is in range.
+	 * @param number
 	 * @return
 	 */
-	public boolean nameLength(String name) {
-		if (name.matches("........") || name.length() <= 8) {
+	public boolean isInRange(String number) {
+		if (Integer.parseInt(number) > 0 && Integer.parseInt(number) <= 8) {
 			return true;
 		}
-		else {
+		return false;
+	}
+		
+	/**
+	 * Checks to see if the string is empty. No input or all whitespace.
+	 * @param string
+	 * @return
+	 */
+	public boolean isEmpty(String string) {
+		if(string.isBlank()) {
 			return false;
 		}
+		return true;
 	}
 }
